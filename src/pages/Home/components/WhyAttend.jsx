2329fdb1212e41dd754e71df110a3adf996c4fc1
elmoustafi-22 <mustophaqadir@gmail.com>
2025-09-18 22:53:31 +0100

    fix

--- src/pages/Home/components/WhyAttend.jsx
+++ src/pages/Home/components/WhyAttend.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WhyAttend = () => {
   return (
-    <section className="bg-white md:container px-4 md:px-6 mt-6 md:mt-10">
+    <section className="bg-white md:container px-6 mt-6 md:mt-10">
       <div className="flex md:justify-between gap-3 md:gap-4 flex-col md:flex-row">
         <h1 className="text-left text-prussian-blue text-2xl md:text-4xl lg:text-5xl font-heading font-bold tracking-tight">
           Why Attend?
@@ -17,7 +17,7 @@ const WhyAttend = () => {
           </span>
         </p>
       </div>
-      <div className="px-0 md:px-5 lg:px-30 grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-2 mt-6 md:mt-12">
+      <div className="px-0 md:px-8 lg:px-30 grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-2 mt-6 md:mt-12">
         <div className="relative rounded-xl shadow flex flex-col gap-4 md:gap-8 justify-between shadow-blue-200 px-4 md:px-8 py-4 md:py-6 bg-royal-blue/20 overflow-hidden">
           <h3 className="font-heading text-xl md:text-4xl font-medium text-prussian-blue">
             Cutting edge insights
