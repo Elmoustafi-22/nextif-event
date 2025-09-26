89c05a6e790504fb21d768a381ffea503db3c8eb
elmoustafi-22 <mustophaqadir@gmail.com>
2025-09-26 21:18:40 +0100

    update Agenda component with new topics and descriptions for improved content relevance

--- src/pages/Home/components/Agenda.jsx
+++ src/pages/Home/components/Agenda.jsx
@@ -2,24 +2,36 @@ import React from "react";
 
 const Agenda = () => {
 const agendaItems = [
-    {
-        day: "Day 1",
-        time: "9:00 - 3:00",
-        title: "Master Halal Fintech Models",
-        description: "Understand profit-sharing, asset-backed financing, and Sukuk structures that comply with Islamic principles"
-    },
-    {
-        day: "Day 2",
-        time: "9:00 - 2:00",
-        title: "Access Capital Networks",
-        description: "Connect with Islamic investors, VCs, and accelerators actively funding halal startups"
-    },
-    {
-        day: "Day 3",
-        time: "10:00 - 12:00",
-        title: "Build Sustainable Business Models",
-        description: "Design fintech solutions that serve the 1.8 billion Muslim market while staying profitable"
-    }
+  {
+    topic: "Topic 1",
+    title: "Halal Economy & Innovation",
+    description:
+      "Redefining the Halal Economy: Where Innovation Meets Billions in Untapped Opportunities",
+  },
+  {
+    topic: "Topic 2",
+    title: "Entrepreneurs & Halal",
+    description:
+      "Entrepreneurship with Barakah: Building Halal Enterprises That Outlive Founders",
+  },
+  {
+    topic: "Topic 3",
+    title: "Islamic Finance Tech",
+    description:
+      "Faith in Code: How Islamic Fintech is Rewiring the Future of Global Finance",
+  },
+  {
+    topic: "Topic 4",
+    title: "Startups & Investors",
+    description:
+      "Bridging Startups and Capital: What Investors Look for in Shariah-Compliant Ventures",
+  },
+  {
+    topic: "Topic 5",
+    title: "Shariah-Compliance ",
+    description:
+      "In the New Dawn of Business, Compliance is the Currency of Scale",
+  },
 ];
 
 return (
@@ -43,8 +55,7 @@ return (
             className="flex flex-col md:flex-row gap-2 justify-between"
           >
             <p className="text-2xl md:text-3xl font-heading text-prussian-blue font-semibold flex flex-col">
-              <span>{item.day}</span>
-              <span>{item.time}</span>
+              <span>{item.topic}</span>
             </p>
             <p className="mt-4 md:mt-0 flex flex-col gap-2 md:gap-8 justify-start w-full md:w-2/3 text-lg font-body">
               <span className="text-2xl md:text-3xl font-heading text-prussian-blue font-semibold ">
