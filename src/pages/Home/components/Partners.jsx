2b66b13f6801f6abad3a8b74af27ba2fcb1e200e
elmoustafi-22 <mustophaqadir@gmail.com>
2025-11-04 20:10:53 +0100

    add: add SHURIX in Partners component

--- src/pages/Home/components/Partners.jsx
+++ src/pages/Home/components/Partners.jsx
@@ -22,8 +22,8 @@ const Partners = () => {
       logo: "/images/ifing.png",
     },
     {
-      name: "ABUMFLY",
-      logo: "/images/abumfly.jpg",
+      name: "SHURIX",
+      logo: "/images/shurix.jpg",
     },
     {
       name: "MTECH",
